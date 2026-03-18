c3a1e6223cf87e24ebd67316fa1a9b9f1502296b
angelhugo <angelpilares2003@hotmail.com>
2026-03-18 17:55:58 -0500

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-// app.js let appState={ currentScript:null, writingMode:false,
-saveState:“idle” };
-
-function toggleWritingMode(){
-appState.writingMode=!appState.writingMode; const
-btn=document.getElementById(“writingModeBtn”); btn.textContent=“Modo
-escritura:”+(appState.writingMode?“ON”:“OFF”); }
